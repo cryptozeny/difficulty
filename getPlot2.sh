d9b55f3be6f5d5d9c53993c7bdfe9d140b1e734f
cryptozeny <37016180+cryptozeny@users.noreply.github.com>
2019-01-16 21:01:08 +0900

    add: CHAIN_TYPE (main, test, regtest) & more
    add: 
    CHAIN_TYPE
    DIFF_N_SIZE
    MA_SIZE_1
    MA_SIZE_2
    
    change:
    POW_LIMIT : RPC call
    GET_GENESIS_OFFSET : Since it's not global, so I changed variable names.

--- getPlot2.sh
+++ getPlot2.sh
@@ -1,17 +1,38 @@
-#190112 #MA
 #!/bin/bash
 
-# INTERVAL_OFFSET=-6171284
+# init
+COIN_CLI="$HOME/git/SUGAR/WALLET/sugarchain-v0.16.3/src/sugarchain-cli"
+COIN_OPTION="-rpcuser=username -rpcpassword=password -testnet"
+
+CHAIN_TYPE=$( $COIN_CLI $COIN_OPTION getblockchaininfo | jq -r '[.chain] | "\(.[0])"' )
+
+BLOCK_TIME="5"
+COIN_NAME="$CHAIN_TYPE.Sugarchain(t$BLOCK_TIME)"
+POW_NAME="YP"
+DIFF_NAME="DS"
+DIFF_N_SIZE="510"
+
+GENESIS_HASH=$( $COIN_CLI $COIN_OPTION getblockhash 0 )
+
+# POW_LIMIT="1.192074847720173e-07"
+POW_LIMIT=$( $COIN_CLI $COIN_OPTION getblock $GENESIS_HASH | jq -r '[.difficulty] | "\(.[0])"' )
+
+# TOTAL_BLOCK_AMOUNT=600 # test
+TOTAL_BLOCK_AMOUNT=$($COIN_CLI $COIN_OPTION getblockcount)
+
+FILE_NAME="$COIN_NAME-$POW_NAME-$DIFF_NAME(n$DIFF_N_SIZE).csv"
+DIFF_INIT=$(CONVERT_SCIENTIFIC_NOTATION $POW_LIMIT)
+
 function GET_GENESIS_OFFSET() {
     #statements
-    GENESIS_HASH=$( $COIN_CLI $COIN_OPTION getblockhash 0 )
-    GENESIS_DATA=$( $COIN_CLI $COIN_OPTION getblock $GENESIS_HASH | jq -r '[.time, .difficulty, .previousblockhash] | "\(.[0]) \(.[1]) \(.[2])"' )
-    GENESIS_TIME=$( echo $GENESIS_DATA | awk '{print $1}' )
-    FIRST_HASH=$( $COIN_CLI $COIN_OPTION getblockhash 1 )
-    FIRST_DATA=$( $COIN_CLI $COIN_OPTION getblock $FIRST_HASH | jq -r '[.time, .difficulty, .previousblockhash] | "\(.[0]) \(.[1]) \(.[2])"' )
-    FIRST_TIME=$( echo $FIRST_DATA | awk '{print $1}' )
-    GENESIS_OFFSET=$(( $FIRST_TIME - $GENESIS_TIME ))
-    echo $GENESIS_OFFSET
+    genesisHash=$( $COIN_CLI $COIN_OPTION getblockhash 0 )
+    genesisData=$( $COIN_CLI $COIN_OPTION getblock $genesisHash | jq -r '[.time, .difficulty, .previousblockhash] | "\(.[0]) \(.[1]) \(.[2])"' )
+    genesisTime=$( echo $genesisData | awk '{print $1}' )
+    firstHash=$( $COIN_CLI $COIN_OPTION getblockhash 1 )
+    firstData=$( $COIN_CLI $COIN_OPTION getblock $firstHash | jq -r '[.time, .difficulty, .previousblockhash] | "\(.[0]) \(.[1]) \(.[2])"' )
+    firstTime=$( echo $firstData | awk '{print $1}' )
+    genesisOffset=$(( $firstTime - $genesisTime ))
+    echo $genesisOffset
 }
 
 function CONVERT_SCIENTIFIC_NOTATION() {
@@ -20,24 +41,12 @@ function CONVERT_SCIENTIFIC_NOTATION() {
     echo ${@} | sed 's#\([+-]\{0,1\}[0-9]*\.\{0,1\}[0-9]\{1,\}\)[eE]+\{0,1\}\(-\{0,1\}\)\([0-9]\{1,\}\)#(\1*10^\2\3)#g' | bc -l
 }
 
-# init
-POW_LIMIT="1.192074847720173e-07"
-COIN_NAME="SugarchainTestnetT5"
-POW_NAME="YesPower10Sugar"
-DIFF_NAME="DigiShieldN255"
-COIN_CLI="$HOME/git/SUGAR/WALLET/sugarchain-v0.16.3/src/sugarchain-cli"
 
 # check COIN_CLI
 if [ ! -e $COIN_CLI ]; then
     echo "ERROR: NO COIN_CLI: $COIN_CLI"
     exit 1
 fi
-
-COIN_OPTION="-rpcuser=username -rpcpassword=password -testnet"
-TOTAL_BLOCK_AMOUNT=$($COIN_CLI $COIN_OPTION getblockcount)
-# TOTAL_BLOCK_AMOUNT=300 # test
-FILE_NAME="$COIN_NAME-$POW_NAME-$DIFF_NAME.csv"
-DIFF_INIT=$(CONVERT_SCIENTIFIC_NOTATION $POW_LIMIT)
 
 # new? or continue?
 if [ ! -e $FILE_NAME ]; then
@@ -83,37 +92,41 @@ do
 done
 
 # remove ma file before calculate for clean result
-if [ -e $FILE_NAME.MA-255 ]; then
+if [ -e $FILE_NAME.MA-$DIFF_N_SIZE ]; then
     rm $FILE_NAME.MA-*
 fi 
 
 # calculate MA
 # https://rosettacode.org/wiki/Averages/Simple_moving_average#AWK
 # P = MA_WINDOW
-awk '{print $1, $5}' $FILE_NAME | awk 'BEGIN { P = 255; } { x = $2; i = NR % P; MA += (x - Z[i]) / P; Z[i] = x; print $1, MA; }' > $FILE_NAME.MA-255
-awk '{print $1, $5}' $FILE_NAME | awk 'BEGIN { P = 85; } { x = $2; i = NR % P; MA += (x - Z[i]) / P; Z[i] = x; print $1, MA; }' > $FILE_NAME.MA-85
-awk '{print $1, $5}' $FILE_NAME | awk 'BEGIN { P = 34; } { x = $2; i = NR % P; MA += (x - Z[i]) / P; Z[i] = x; print $1, MA; }' > $FILE_NAME.MA-34
+
+MA_SIZE_1="170"
+MA_SIZE_2="34"
+
+awk '{print $1, $5}' $FILE_NAME | awk -v DIFF_N_SIZE="$DIFF_N_SIZE" 'BEGIN { P = DIFF_N_SIZE; } { x = $2; i = NR % P; MA += (x - Z[i]) / P; Z[i] = x; print $1, MA; }' > $FILE_NAME.MA-$DIFF_N_SIZE
+awk '{print $1, $5}' $FILE_NAME | awk -v MA_SIZE="$MA_SIZE_1" 'BEGIN { P = MA_SIZE; } { x = $2; i = NR % P; MA += (x - Z[i]) / P; Z[i] = x; print $1, MA; }' > $FILE_NAME.MA-$MA_SIZE_1
+awk '{print $1, $5}' $FILE_NAME | awk -v MA_SIZE="$MA_SIZE_2" 'BEGIN { P = MA_SIZE; } { x = $2; i = NR % P; MA += (x - Z[i]) / P; Z[i] = x; print $1, MA; }' > $FILE_NAME.MA-$MA_SIZE_2
 
 # plot parameters
 SET_XRANGE="[0:*]"
 SET_YRANGE="[0:10]"
-SET_Y2RANGE="[$POW_LIMIT:$POW_LIMIT*4]"
+SET_Y2RANGE="[$POW_LIMIT:$POW_LIMIT*3]"
 
 # plot draw
 gnuplot -persist <<-EOFMarker 
 set title "$FILE_NAME"; set term qt size 1200, 600;
 set label 1 "LIMIT = $POW_LIMIT"; set label 1 at graph 0.015, 0.94 tc rgb "black";
 set label 2 "BLOCKS = $TOTAL_BLOCK_AMOUNT"; set label 2 at graph 0.015, 0.88 tc rgb "black";
-set xrange $SET_XRANGE; set xlabel "Block Number"; set xtics 0, 255*2 rotate by 45 right; set xtics add ("N=255" 255);
+set xrange $SET_XRANGE; set xlabel "Block Number"; set xtics 0, 17*60 rotate by 45 right; set xtics add ("GENESIS" 0) ("N+2=512" 512);
 set yrange $SET_YRANGE; set ylabel "Block Time"; set ytics 0, 1; set ytics nomirror;
-set y2range $SET_Y2RANGE; set y2label "Difficulty"; set format y2 '%.3g'; set y2tics $POW_LIMIT, $POW_LIMIT/2;
+set y2range $SET_Y2RANGE; set y2label "Difficulty"; set format y2 '%.3g'; set y2tics $POW_LIMIT, $POW_LIMIT/5;
 # set grid xtics ytics y2tics mxtics mytics my2tics;
-set grid xtics ytics mxtics mytics;
+set grid xtics ytics;
 set key invert; # reverse key order
 plot \
-"$FILE_NAME.MA-34" using 0:2 axis x1y1 w l title "(MA-34) Block Time" lc rgb "#eeeeee" lw 1.0, \
-"$FILE_NAME.MA-85" using 0:2 axis x1y1 w l title "(MA-85) Block Time" lc rgb "#cccccc" lw 1.0, \
-"$FILE_NAME.MA-255" using 0:2 axis x1y1 w l title "(MA-255) Block Time" lc rgb "black" lw 1.25, \
+"$FILE_NAME.MA-$MA_SIZE_2" using 0:2 axis x1y1 w l title "(MA-$MA_SIZE_2) Block Time" lc rgb "#eeeeee" lw 1.0, \
+"$FILE_NAME.MA-$MA_SIZE_1" using 0:2 axis x1y1 w l title "(MA-$MA_SIZE_1) Block Time" lc rgb "#cccccc" lw 1.0, \
+"$FILE_NAME.MA-$DIFF_N_SIZE" using 0:2 axis x1y1 w l title "(MA-$DIFF_N_SIZE) Block Time" lc rgb "black" lw 1.25, \
 "$FILE_NAME" using 1:3 axis x1y2 w l title "Difficulty" lc rgb "red" lw 1.25,
 # caution at the end: no "\"
 EOFMarker
