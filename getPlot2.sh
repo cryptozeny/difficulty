9e505d8c234a203dcc8507a75ca0c48358356cc9
cryptozeny <37016180+cryptozeny@users.noreply.github.com>
2019-01-25 19:43:15 +0900

    fix: some ratio for gnuplot

--- getPlot2.sh
+++ getPlot2.sh
@@ -3,7 +3,7 @@
 
 # init
 COIN_CLI="$HOME/git/SUGAR/WALLET/sugarchain-v0.16.3/src/sugarchain-cli"
-COIN_OPTION="-rpcuser=username -rpcpassword=password -testnet" # MAIN: nothing | TESTNET: -testnet | REGTEST: -regtest
+COIN_OPTION="-rpcuser=username -rpcpassword=password -main" # MAIN: -main | TESTNET: -testnet | REGTEST: -regtest
 GET_INFO="$COIN_CLI $COIN_OPTION"
 
 CHAIN_TYPE=$( $GET_INFO getblockchaininfo | jq -r '[.chain] | "\(.[0])"' )
@@ -17,6 +17,7 @@ DIFF_N_SIZE="510"
 GENESIS_HASH=$( $GET_INFO getblockhash 0 )
 
 # POW_LIMIT="1.192074847720173e-07"
+# POW_LIMIT="2.384149979653205e-07"
 POW_LIMIT=$( $GET_INFO getblock $GENESIS_HASH | jq -r '[.difficulty] | "\(.[0])"' )
 
 # TOTAL_BLOCK_AMOUNT=600 # test
@@ -51,14 +52,14 @@ fi
 # new? or continue?
 if [ ! -e $FILE_NAME ]; then
     # echo "NEW: $FILE_NAME"
-    echo -e "\e[32mNEW: \t$FILE_NAME\e[39m"
+    echo -e "\e[32mNEW: \t$FILE_NAME\e[39m" # green
     START_BLOCK=1
     INTERVAL_TOTAL=0
 else 
     # echo "CONTINUE: $FILE_NAME"
-    echo -e "\e[36mKEEP: \t$FILE_NAME\e[39m"
+    echo -e "\e[36mKEEP: \t$FILE_NAME\e[39m" # cyan
     START_BLOCK=$(( $( tail -n1 $FILE_NAME | awk '{print $1}' ) + 1 ))
-    echo -e "\e[36mCONTINUE FROM $START_BLOCK\e[39m"
+    echo -e "\e[36mCONTINUE FROM $START_BLOCK\e[39m" # cyan
     INTERVAL_TOTAL=$(( $( tail -n1 $FILE_NAME | awk '{print $6}' ) + 0 ))
 fi 
 
@@ -108,19 +109,27 @@ awk '{print $1, $5}' $FILE_NAME | awk -v MA_SIZE="$MA_SIZE_2" 'BEGIN { P = MA_SI
 
 # plot parameters
 SET_XRANGE="[1:*]"
-SET_YRANGE="[0:10]"
-SET_Y2RANGE="[0:$POW_LIMIT*5]"
+SET_YRANGE="[0:5*2]"
+
+PL_RATIO="2"
+# SET_Y2RANGE="[$POW_LIMIT:$POW_LIMIT*5]"
+SET_Y2RANGE="[$POW_LIMIT:$POW_LIMIT*6]"
 
 # plot draw
 gnuplot -persist <<-EOFMarker 
-set title "$FILE_NAME" offset -58; set term qt size 1200, 600;
-set label 1 "LIMIT = $POW_LIMIT"; set label 1 at graph 0.81, 1.03 tc rgb "black";
-set label 2 "BLOCKS = $TOTAL_BLOCK_AMOUNT"; set label 2 at graph 0.81, 1.06 tc rgb "black";
-set xrange $SET_XRANGE; set xlabel "Block Number"; set xtics 1, 17*300 rotate by 45 right; set xtics add ("1" 1) ("N+1=511" 511);
-set yrange $SET_YRANGE; set ylabel "Block Time"; set ytics 0, 1; set ytics nomirror;
-set y2range $SET_Y2RANGE; set y2label "Difficulty"; set format y2 '%.3g'; set y2tics 0, $POW_LIMIT/2; set y2tics add ("{/:Bold LIMIT}" $POW_LIMIT);
+set term qt size 1200, 600;
+set title "BLOCKS=$TOTAL_BLOCK_AMOUNT       FILE=$FILE_NAME       LIMIT=$POW_LIMIT" offset -30;
+# set label 1 "LIMIT = $POW_LIMIT"; set label 1 at graph 0.81, 1.03 tc rgb "black";
+# set label 2 "BLOCKS = $TOTAL_BLOCK_AMOUNT"; set label 2 at graph 0.81, 1.06 tc rgb "black";
+set xlabel "Block Number";
+set xrange $SET_XRANGE; set xtics 1, 17*50 rotate by 45 right; set xtics add ("1" 1) ("N+1=511" 511);
+set ylabel "Block Time";
+set yrange $SET_YRANGE; set ytics 0, 1; set ytics add ($BLOCK_TIME);
+set ytics nomirror;
+set y2label "Difficulty" tc rgb "red";
+set y2range $SET_Y2RANGE; set format y2 '%.3g'; set y2tics $POW_LIMIT, $POW_LIMIT/$PL_RATIO; set y2tics add ($POW_LIMIT);
 # set grid xtics ytics y2tics mxtics mytics my2tics;
-set grid xtics ytics;
+set grid xtics y2tics;
 set key top left invert; set key box opaque;
 plot \
 "$FILE_NAME.MA-$MA_SIZE_2" using 0:2 axis x1y1 w l title "(MA-$MA_SIZE_2) Block Time" lc rgb "#eeeeee" lw 1.0, \
